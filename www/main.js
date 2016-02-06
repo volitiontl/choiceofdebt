a7e58968da43bb8696abf2349d281a98abdba7f9
Tony Le <tony_le_1@hotmail.com>
2016-02-05 22:26:00 -0500

    rough layout

--- www/main.js
+++ www/main.js
@@ -10,10 +10,37 @@
 angular.module("app", ['ngRoute'])
   .controller("main", function () {
   })
+  .controller("review",function($scope,$http){
+    transactions = [];
+
+    $scope.currentTransaction=false
+
+
+    $http.get('mock/bank.json')
+      .then(function (response) {
+        transactions = response.data.spending
+        $scope.currentTransaction=transactions && transactions.length>0 &&transactions[0]
+
+      })
+
+    $scope.classify=function(type){
+
+      $scope.currentTransaction=transactions.pop()
+      pushToFirebase()
+      //update firebase
+    }
+
+
+    var pushToFirebase= _.noop
+
+  })
+
   .config(['$routeProvider',
     function ($routeProvider) {
       $routeProvider
         .when('/', {templateUrl: 'templates/landingpage.html', controller: 'main'})
         .when('/signin', {templateUrl: 'templates/signin.html', controller: 'main'})
+        .when('/review', {templateUrl: 'templates/review.html', controller: 'review'})
+        .when('/choices', {templateUrl: 'templates/choices.html', controller: 'main'})
     }]
 )
