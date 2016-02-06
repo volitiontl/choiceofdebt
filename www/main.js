e0166d102a497644de42f58056da2d3cdce9e6ab
Tony Le <tony_le_1@hotmail.com>
2016-02-06 08:19:42 -0500

    using high charts bubble chart

--- www/main.js
+++ www/main.js
@@ -7,25 +7,55 @@
 //injectInstance.load("main")
 
 
-angular.module("app", ['ngRoute'])
+angular.module("app", ['ngRoute','highcharts-ng'])
   .controller("main", function ($rootScope) {
 
   })
   .controller("visualize",function($scope){
 
+    $scope.highchartsNG={
+      plotOptions: {
+        series: {
+          //animation: false
+          duration: 0
+        },
+        bubble: {
+          minSize: 3,
+          maxSize: 3
+        }
+      },
+      options: {
+        chart: {
+          type: 'bubble'
+        }
+      },
+      legend: {
+        enabled: false
+      },
+      yAxis:{min: -1000, max: 1000},
+      xAxis:{min:-2,max:20},
 
+      series: [{
+        data: [],
+        sizeBy: 'width',
+        enableMouseTracking: false
+      }],
+      title: {
+        text: ''
+      },
+      loading: false
+    }
+
+    var tt=$scope.highchartsNG.series[0].data
     var events=[
-      {id:0,income:1,net:0,time:0,name:"part-time job"},
-      {id:0,income:0,net:-10,time:10,name:"going to school"},
-      {id:0,income:0,net:-10,time:20,name:"another loan"},
-      {id:0,income:1,net:0,time:100,name:""},
+      {id:0,income:0.5,net:0,time:0,name:"part-time job"},
+      {id:0,income:3,net:0,time:10,name:"going to school"},
       {id:2,income:0,net:-10,time:11,name:""},
       {id:2,income:0,net:-10,time:23,name:""},
-      {id:2,income:0,net:-10,time:104,name:""}
+      {id:3,income:0,net:-400,time:0,name:""},
+      {id:4,income:3.5,net:-400,time:0,name:""},
+      {id:5,income:9,net:-400,time:0,name:""}
     ]
-
-
-
 
     var temp=_(events)
       .groupBy("id")
@@ -37,41 +67,38 @@ angular.module("app", ['ngRoute'])
     var eventsIndex= _.indexBy(events,"time")
     var delay=20;
     var i=0;
-    var duration=500
+    var duration=100
     var loopInterval;
 
     function update(){
       i=0;
-
-      $scope.balls=_(temp)
-        .map(function(a,b){
-          return {id:b,x:0,y:0,color:"red",size:10,income:0,net:0,eventsIndex:a}
-        })
-        .value()
-
+      _.forEach(temp, function (a, b) {
+        tt[b] = {id: b, x: 0, y: 0, z: 0, size: 1, income: 0, net: 0, eventsIndex: a, name: "aa"}
+      })
+      console.log(temp)
     }
     update()
 
-    function process(a) {
 
+    function process(a,b) {
       if (a.eventsIndex[i]) {
         a.income = eventsIndex[i].income
         a.net += eventsIndex[i].net
       }
-
-      a.x = a.income*20
       a.net+= a.income
-      a.y = -a.net+200
+      tt[b].x= a.income
+      tt[b].y= a.net
     }
 
 
 
 
     function tick() {
-
-      if (i >= duration) clearInterval(loopInterval);
-      var t = $scope.balls;
-      _.forEach(t, process)
+      if (i >= duration){
+        clearInterval(loopInterval);
+        return
+      }
+      _.forEach(tt, process)
       i++
       $scope.$apply()
     }
